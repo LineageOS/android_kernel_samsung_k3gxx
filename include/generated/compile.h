ae9e9c890aecf6b8ec3372ae2aebce724e326bb6
Griffin98 <griffin98@protonmail.com>
2018-01-13 21:49:36 +0530

    k3gxx:cleanup the build

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Jan 13 18:00:59 IST 2018"
-#define LINUX_COMPILE_BY "dhyey"
-#define LINUX_COMPILE_HOST "Arch"
-#define LINUX_COMPILER "gcc version 4.8 (GCC) "
